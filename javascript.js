860d83cd98707c859bf5e53b09c734eb3ecd154b
YvonneYoobee <270164654@yoobeestudent.ac.nz>
2022-02-22 11:16:11 +1300

    testttt

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
+let utku_div = document.getElementById("utku_div");
+let utku_hidden = document.getElementById("utku_hidden");
+utku_div.onclick = utku_hidden.classList.toggle("hidden");
 
 let yvonne_div = document.getElementById("yvonne_div");
 let yvonne_hidden = document.getElementById("yvonne_hidden");
