1afff8ffd707be3f1f6b884f043627d9160c3c18
YvonneYoobee <270164654@yoobeestudent.ac.nz>
2022-02-22 11:11:16 +1300

    ag

--- javascript.js
+++ javascript.js
@@ -1,9 +1,3 @@
-let utku_div = document.getElementById("utku_div");
-let utku_hidden = document.getElementById("utku_hidden");
-utku_div.onclick = utku_hidden.classList.toggle("hidden");
-
-
-
 
 
 
