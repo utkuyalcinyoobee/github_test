1c48e84bae63082f86cd8fbb2494007efcc95a4c
YvonneYoobee <270164654@yoobeestudent.ac.nz>
2022-02-22 11:14:35 +1300

    tst

--- javascript.js
+++ javascript.js
@@ -1,7 +1,4 @@
 
-
-
-
 let yvonne_div = document.getElementById("yvonne_div");
 let yvonne_hidden = document.getElementById("yvonne_hidden");
 yvonne_div.onclick = yvonne_hidden.classList.toggle("hidden");
